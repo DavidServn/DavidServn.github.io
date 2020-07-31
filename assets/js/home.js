e900a62cd3e413ffd1ca9f4181acdc1f8f186305
David Servin <davidservn@hotmail.com>
2020-07-30 21:02:40 -0500

    this is a version

---
+++ assets/js/home.js
@@ -0,0 +1,25 @@
+(function($) {
+
+	skel.breakpoints({
+		xlarge:		'(max-width: 1680px)',
+		large:		'(max-width: 1280px)',
+		medium:		'(max-width: 980px)',
+		small:		'(max-width: 736px)',
+		xsmall:		'(max-width: 480px)',
+		xxsmall:	'(max-width: 360px)'
+	});
+
+	$(function() {
+
+        var	$avatar = $('.avatar')
+
+        $("a").hover(
+            function() {
+                $avatar.css( "background-image", "url('assets/images/photo-pixel-glitch.gif')" );
+            }, function() {
+                $avatar.css( "background-image", "url('assets/images/photo-pixel.gif')" );
+            }
+          );
+	});
+
+})(jQuery);
