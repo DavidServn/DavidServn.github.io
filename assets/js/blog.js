3657ea4179da68a78fc2ebc1dce4da6856c14adf
David Servin <davidservn@hotmail.com>
2021-03-05 23:32:46 -0600

    test: blog

---
+++ assets/js/blog.js
@@ -0,0 +1,16 @@
+(function($) {
+
+	skel.breakpoints({
+		xlarge:		'(max-width: 1680px)',
+		large:		'(max-width: 1280px)',
+		medium:		'(max-width: 980px)',
+		small:		'(max-width: 736px)',
+		xsmall:		'(max-width: 480px)',
+		xxsmall:	'(max-width: 360px)'
+	});
+
+	$(function() {
+		$('body').addClass('loaded');
+	});
+
+})(jQuery);
